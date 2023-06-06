820e40ee51ebff1b6bc554b45cc141a92f3e3b50
Florian Bacher <florian.bacher@dynatrace.com>
2023-06-06 12:45:35 +0200

    add concurrent requests

--- grpc-test/testsuite.go
+++ grpc-test/testsuite.go
@@ -7,7 +7,11 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/protobuf/types/known/structpb"
 	"k8s.io/apimachinery/pkg/util/rand"
+	"os"
+	"strconv"
+	"sync"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -17,40 +21,49 @@ import (
 )
 
 var _ = Describe("YourGRPCService", func() {
-	var (
-		conn   *grpc.ClientConn
-		client pb.ServiceClient
-	)
 
-	BeforeEach(func() {
-		var err error
-		conn, err = grpc.Dial("flagd.flagd-performance-test:8013", grpc.WithTransportCredentials(insecure.NewCredentials()))
-		Expect(err).NotTo(HaveOccurred())
-		client = pb.NewServiceClient(conn)
-	})
+	It("should perform gRPC requests successfully", func() {
 
-	AfterEach(func() {
-		conn.Close()
-	})
-
-	It("should perform a gRPC request successfully", func() {
-
-		for i := 0; i < 1000; i++ {
-			randNumber := rand.Intn(5000)
-			resp, err := client.ResolveString(context.Background(), &schemav1.ResolveStringRequest{
-				FlagKey: fmt.Sprintf("color-%d", randNumber),
-				Context: &structpb.Struct{
-					Fields: map[string]*structpb.Value{
-						"version": structpb.NewStringValue("1.0.0"),
-					},
-				},
-			})
-			Expect(err).NotTo(HaveOccurred())
-			Expect(resp).NotTo(BeNil())
+		numClients := 1000
+		if numClientsEnv := os.Getenv("NUM_CLIENTS"); numClientsEnv != "" {
+			if parsedNumClients, err := strconv.ParseInt(numClientsEnv, 10, 64); err != nil && parsedNumClients > 0 {
+				numClients = int(parsedNumClients)
+			}
 		}
+		wg := &sync.WaitGroup{}
+
+		wg.Add(numClients)
+
+		for i := 0; i < numClients; i++ {
+			go doRequests(wg)
+		}
+
+		wg.Wait()
 
 	})
 })
+
+func doRequests(wg *sync.WaitGroup) {
+	conn, err := grpc.Dial("flagd.flagd-performance-test:8013", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	Expect(err).NotTo(HaveOccurred())
+	client := pb.NewServiceClient(conn)
+	for i := 0; i < 100000; i++ {
+		randNumber := rand.Intn(5000)
+		resp, err := client.ResolveString(context.Background(), &schemav1.ResolveStringRequest{
+			FlagKey: fmt.Sprintf("color-%d", randNumber),
+			Context: &structpb.Struct{
+				Fields: map[string]*structpb.Value{
+					"version": structpb.NewStringValue("1.0.0"),
+				},
+			},
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(resp).NotTo(BeNil())
+		<-time.After(10 * time.Millisecond)
+	}
+	conn.Close()
+	wg.Done()
+}
 
 func TestGRPC(t *testing.T) {
 	RegisterFailHandler(Fail)
