80899ac5cff648cbd6db48d357b9914cf12289f6
Florian Bacher <florian.bacher@dynatrace.com>
2023-06-13 15:08:17 +0200

    adapt ports

--- grpc-test/testsuite.go
+++ grpc-test/testsuite.go
@@ -138,7 +138,7 @@ func doHttpRequests(duration time.Duration) {
 
 		marshal, _ := json.Marshal(reqBody)
 
-		resp, err := http.Post("http://flagd-http.flagd-performance-test:80/schema.v1.Service/ResolveString", "application/json", bytes.NewReader(marshal))
+		resp, err := http.Post("http://flagd-http.flagd-performance-test:8013/schema.v1.Service/ResolveString", "application/json", bytes.NewReader(marshal))
 
 		Expect(err).NotTo(HaveOccurred())
 		Expect(resp).NotTo(BeNil())
@@ -150,7 +150,7 @@ func doHttpRequests(duration time.Duration) {
 }
 
 func establishGrpcConnection() (*grpc.ClientConn, pb.ServiceClient) {
-	conn, err := grpc.Dial("flagd.flagd-performance-test:80", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial("flagd.flagd-performance-test:8013", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	Expect(err).NotTo(HaveOccurred())
 	client := pb.NewServiceClient(conn)
 	return conn, client
